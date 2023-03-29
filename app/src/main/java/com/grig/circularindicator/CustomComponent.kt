5c06e9d9093b8a17eda047f2d754350b28fb26e8
grig <bob>
2023-03-29 20:23:19 +0300

    Finished Commit

--- app/src/main/java/com/grig/circularindicator/CustomComponent.kt
+++ app/src/main/java/com/grig/circularindicator/CustomComponent.kt
@@ -1,9 +1,16 @@
 package com.grig.circularindicator
 
+import androidx.compose.animation.animateColorAsState
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.animation.core.animateIntAsState
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.size
 import androidx.compose.material.MaterialTheme
-import androidx.compose.runtime.Composable
+import androidx.compose.material.Text
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.drawBehind
 import androidx.compose.ui.geometry.Offset
@@ -12,6 +19,9 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.StrokeCap
 import androidx.compose.ui.graphics.drawscope.DrawScope
 import androidx.compose.ui.graphics.drawscope.Stroke
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
@@ -23,20 +33,93 @@ fun CustomComponent(
     maxIndicatorValue: Int = 100,
     indicatorColor: Color = MaterialTheme.colors.onSurface.copy(alpha = 0.1f),
     indicatorStrokeWidth: Float = 100f,
-    sweepAngle: Float = 240f
+    indicatorStrokeCap: StrokeCap = StrokeCap.Round,
+    sweepAngle: Float = 240f,
+    foregroundIndicatorColor: Color = MaterialTheme.colors.primary,
+    foregroundIndicatorStrokeWidth: Float = 100f,
+    bigTextSuffix: String = "GB",
+    smallTextStyle: TextStyle = MaterialTheme.typography.h6.copy(
+        color = MaterialTheme.colors.onSurface.copy(
+            alpha = 0.3f
+        )
+    ),
+    bigTextStyle: TextStyle = MaterialTheme.typography.h3,
+    smallText: String = "Remaining",
+    duration: Int = 1000,
 ) {
-    Column(modifier = Modifier
-        .size(canvasSize)
-        .drawBehind {
-            val componentSize = size / 1.25f
-            backgroundIndicator(
-                componentSize = componentSize,
-                indicatorColor = indicatorColor,
-                indicatorStrokeWidth = indicatorStrokeWidth,
-                sweepAngle = sweepAngle
-            )
-        }
+    var allowedIndicatorValue by remember {
+        mutableStateOf(maxIndicatorValue)
+    }
+
+    allowedIndicatorValue =
+        if (indicatorValue <= maxIndicatorValue) indicatorValue else maxIndicatorValue
+
+    val startAngle = (360 - sweepAngle) / 2 + 90 // calculate start angle
+
+    var animatedIndicatorValue by remember {
+        mutableStateOf(0f)
+    }
+    LaunchedEffect(key1 = allowedIndicatorValue) {
+        animatedIndicatorValue = allowedIndicatorValue.toFloat()
+    }
+
+    val portion = (animatedIndicatorValue / maxIndicatorValue)
+
+    val animatedSweepAngle by animateFloatAsState(
+        targetValue = (sweepAngle * portion),
+        animationSpec = tween(duration)
+    )
+
+    val animatedReceiveValue by animateIntAsState(
+        targetValue = allowedIndicatorValue,
+        animationSpec = tween(duration)
+    )
+
+//    val animatedBigTextColor by animateColorAsState(
+//        targetValue = if (allowedIndicatorValue == 0) MaterialTheme.colors.onSurface.copy(
+//            alpha = 0.3f
+//        ) else bigTextStyle.color,
+//        animationSpec = tween(duration)
+//    )
+
+    val animatedBigTextColor by animateColorAsState(
+        targetValue = if (allowedIndicatorValue == 0) MaterialTheme.colors.onSurface.copy(alpha = 0.3f)
+         else MaterialTheme.colors.onSurface.copy(0.8f),
+        animationSpec = tween(duration)
+    )
+
+    Box(
+        modifier = Modifier
+            .size(canvasSize)
+            .drawBehind {
+                val componentSize = size / 1.25f
+                backgroundIndicator(
+                    componentSize = componentSize,
+                    indicatorColor = indicatorColor,
+                    indicatorStrokeWidth = indicatorStrokeWidth,
+                    startAngle = startAngle,
+                    sweepAngle = sweepAngle,
+                    indicatorStrokeCap = indicatorStrokeCap
+                )
+                foregroundIndicator(
+                    componentSize = componentSize,
+                    indicatorColor = foregroundIndicatorColor,
+                    indicatorStrokeWidth = foregroundIndicatorStrokeWidth,
+                    startAngle = startAngle,
+//                sweepAngle = sweepAngle
+                    sweepAngle = animatedSweepAngle,
+                    indicatorStrokeCap = indicatorStrokeCap
+                )
+            },
+        contentAlignment = Alignment.Center,
     ) {
+        EmbeddedElements(
+            bigText = animatedReceiveValue,
+            bigTextStyle = bigTextStyle.copy(color = animatedBigTextColor),
+            bigTextSuffix = bigTextSuffix,
+            smallText = smallText,
+            smallTextStyle = smallTextStyle
+        )
     }
 }
 
@@ -44,9 +127,10 @@ fun DrawScope.backgroundIndicator(
     componentSize: Size,
     indicatorColor: Color,
     indicatorStrokeWidth: Float,
-    sweepAngle: Float
+    startAngle: Float,
+    sweepAngle: Float,
+    indicatorStrokeCap: StrokeCap
 ) {
-    val startAngle = (360 - sweepAngle) / 2 + 90 // calculate start angle
     drawArc(
         size = componentSize,
         color = indicatorColor,
@@ -55,7 +139,7 @@ fun DrawScope.backgroundIndicator(
         useCenter = false,
         style = Stroke(
             width = indicatorStrokeWidth,
-            cap = StrokeCap.Round
+            cap = indicatorStrokeCap
         ),
         topLeft = Offset(
             x = (size.width - componentSize.width) / 2,
@@ -64,8 +148,53 @@ fun DrawScope.backgroundIndicator(
     )
 }
 
+fun DrawScope.foregroundIndicator(
+    componentSize: Size,
+    indicatorColor: Color,
+    indicatorStrokeWidth: Float,
+    sweepAngle: Float,
+    startAngle: Float,
+    indicatorStrokeCap: StrokeCap
+) {
+    drawArc(
+        size = componentSize,
+        color = indicatorColor,
+        startAngle = startAngle,
+        sweepAngle = sweepAngle,
+        useCenter = false,
+        style = Stroke(
+            width = indicatorStrokeWidth,
+            cap = indicatorStrokeCap
+        ),
+        topLeft = Offset(
+            x = (size.width - componentSize.width) / 2,
+            y = (size.height - componentSize.height) / 2
+        )
+    )
+}
+
+@Composable
+fun EmbeddedElements(
+    bigText: Int,
+    bigTextStyle: TextStyle,
+    bigTextSuffix: String,
+    smallText: String,
+    smallTextStyle: TextStyle,
+) {
+    Column(horizontalAlignment = Alignment.CenterHorizontally) {
+        Text(
+            text = smallText,
+            style = smallTextStyle.copy(textAlign = TextAlign.Center)
+        )
+        Text(
+            text = "$bigText ${bigTextSuffix.take(2)}",
+            style = bigTextStyle.copy(textAlign = TextAlign.Center, fontWeight = FontWeight.Bold)
+        )
+    }
+}
+
 @Preview(showBackground = true)
 @Composable
 fun CustomComponentPreview() {
-    CustomComponent()
+    CustomComponent(indicatorValue = 30)
 }
--- app/src/main/java/com/grig/circularindicator/MainActivity.kt
+++ app/src/main/java/com/grig/circularindicator/MainActivity.kt
@@ -3,13 +3,20 @@ package com.grig.circularindicator
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.material.TextField
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.core.text.isDigitsOnly
 import com.grig.circularindicator.ui.theme.CircularIndicatorTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,11 +24,37 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             CircularIndicatorTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
+                Column(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .background(MaterialTheme.colors.background),
+                    horizontalAlignment = Alignment.CenterHorizontally
                 ) {
+                    var value by remember {
+                        mutableStateOf(0)
+                    }
+
+                    var text by remember {
+                        mutableStateOf("")
+                    }
+
+                    CustomComponent(
+                        indicatorValue = value,
+                        duration = 2000,
+                        foregroundIndicatorStrokeWidth = 50f,
+//                        foregroundIndicatorColor =
+                    )
+                    TextField(
+                        value = text,
+                        onValueChange = {
+                            if (!it.isDigitsOnly()) return@TextField
+                            value = if (it.isNotEmpty()) it.toInt() else 0
+                            text = it.ifEmpty {""}
+                        },
+                        keyboardOptions = KeyboardOptions(
+                            keyboardType = KeyboardType.Number
+                        )
+                    )
                 }
             }
         }
